ba018a08e3988175192fdeeae46326caa910f36d
Odiesta Shandikarona <shandikaronamade@gmail.com>
2024-02-21 13:30:07 +0800

    finish supabase section

---
+++ src/services/supabase.js
@@ -0,0 +1,7 @@
+import { createClient } from "@supabase/supabase-js";
+const supabaseUrl = "https://lnrxvcwzfsdpxkmgmrll.supabase.co";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imxucnh2Y3d6ZnNkcHhrbWdtcmxsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDgzNTEwNzYsImV4cCI6MjAyMzkyNzA3Nn0.IFxhvz9BbJMk-NIbjwhh9ORRtoCjWLt-3k3KLt2XTRQ";
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
