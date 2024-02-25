e238fb1d1f6c821abda8efaee8decbd538e00586
Odiesta Shandikarona <Shandikaronamade@gmail.com>
2024-02-25 14:33:22 +0800

    finish lecture 355

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,5 +1,5 @@
 import { createClient } from "@supabase/supabase-js";
-const supabaseUrl = "https://lnrxvcwzfsdpxkmgmrll.supabase.co";
+export const supabaseUrl = "https://lnrxvcwzfsdpxkmgmrll.supabase.co";
 const supabaseKey =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imxucnh2Y3d6ZnNkcHhrbWdtcmxsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDgzNTEwNzYsImV4cCI6MjAyMzkyNzA3Nn0.IFxhvz9BbJMk-NIbjwhh9ORRtoCjWLt-3k3KLt2XTRQ";
 const supabase = createClient(supabaseUrl, supabaseKey);
